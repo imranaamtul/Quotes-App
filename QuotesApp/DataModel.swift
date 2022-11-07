843c3250053e36dc11152547e250b240c2c402e5
imrana <azra.imrana@gmail.com>
2022-11-07 05:37:31 -0800

    dataModel

---
+++ QuotesApp/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  QuotesApp
+//
+//  Created by amtul imrana on 07/11/22.
+//
+
+import Foundation
---
+++ QuotesApp/QuotesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  QuotesViewController.swift
+//  QuotesApp
+//
+//  Created by amtul imrana on 07/11/22.
+//
+
+import Foundation
